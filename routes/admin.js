cca4619b51322d85f43c3b46b6f06c2d313099e6
Supriyo Chowdhury <supriyochowdhury760@gmail.com>
2025-06-27 19:26:18 +0530

    Step5, added admin router

---
+++ routes/admin.js
@@ -0,0 +1,40 @@
+const{Router}=require("express");
+const adminRouter = Router();
+
+
+    adminRouter.post("/signup",function(req,res){
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+adminRouter.post("/signin",function(req,res){
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+adminRouter.post("/purchases",function(req,res){
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+adminRouter.put("/course",function(req,res){
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+adminRouter.get("/course/bulk",function(req,res){
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+
+
+
+module.exports={
+    adminRouter:adminRouter
+}
