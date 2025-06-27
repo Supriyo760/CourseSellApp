8cfd47c014c3f61194e5ca868ef1dce92f2525f3
Supriyo Chowdhury <supriyochowdhury760@gmail.com>
2025-06-27 21:05:49 +0530

    Only start if database is up

--- routes/admin.js
+++ routes/admin.js
@@ -14,19 +14,19 @@ adminRouter.post("/signin",function(req,res){
     })
 })
 
-adminRouter.post("/",function(req,res){
+adminRouter.post("/course",function(req,res){
     res.json({
         message:"signup endpoint"
     })
 })
 
-adminRouter.put("/",function(req,res){
+adminRouter.put("/course",function(req,res){
     res.json({
         message:"signup endpoint"
     })
 })
 
-adminRouter.get("/bulk",function(req,res){
+adminRouter.get("/course/bulk",function(req,res){
     res.json({
         message:"signup endpoint"
     })
