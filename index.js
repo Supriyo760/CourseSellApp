d6e6dbdacf1ff028131528839df027fc9bc2fb0c
Supriyo Chowdhury <supriyochowdhury760@gmail.com>
2025-06-27 18:21:13 +0530

    Stop 2- Add skeleton routes for user

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+app.post("/user/signup",function(req,res){
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+app.post("/user/signin",function(req,res){
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+app.post("/user/purchases",function(req,res){
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+app.post("/user/purchase",function(req,res){
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+app.post("/courses",function(req,res){
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+app.listen(3000);
